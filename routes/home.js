3f4e89bb8c0ae520070ecf6b24d6c4640daa38e4
Cheshta31 <cheshtakhurana31@gmail.com>
2024-07-01 08:57:11 +0530

    Updated

--- routes/home.js
+++ routes/home.js
@@ -6,15 +6,15 @@ router.get("/", (req,res) => {
 });
 
 router.get("/home/about-us", (req,res) => {
-	res.render("home/aboutUs", { title: "About Us | Food Aid" });
+	res.render("home/aboutUs", { title: "About Us | Food Share" });
 });
 
 router.get("/home/mission", (req,res) => {
-	res.render("home/mission", { title: "Our mission | Food Aid" });
+	res.render("home/mission", { title: "Our mission | Food Share" });
 });
 
 router.get("/home/contact-us", (req,res) => {
-	res.render("home/contactUs", { title: "Contact us | Food Aid" });
+	res.render("home/contactUs", { title: "Contact us | Food Share" });
 });
 
 
